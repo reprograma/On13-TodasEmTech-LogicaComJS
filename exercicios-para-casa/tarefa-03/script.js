408d07ca61e13fcca94adee9cd18abf716c9bf11
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-09-21 15:22:27 -0300

    Enviando tarefa 02 e tarefa 03

--- exercicios-para-casa/tarefa-03/script.js
+++ exercicios-para-casa/tarefa-03/script.js
@@ -6,3 +6,14 @@
       - caso seja, informe na tela que ela pode comprar bebidas;
       - caso contrário, informe que você não está autorizada a vender álcool para menores.
  */
+
+      //Solicitando ano nascimento
+      let anoNascimento = parseInt(prompt("Informe seu ano de nascimento:"));
+      const maiorIdade = 18;
+      let anoAtual = new Date().getFullYear;
+      let idadePessoa = (anoAtual - anoNascimento);
+      if (idadePessoa >= maiorIdade) {
+        alert("Pode comprar bebidas");
+      } else {
+        alert("Proibido vender álcool para menores de idade");
+      }
