8be262b8bf4b8b479ed811d0f45e138cbf6a7fd4
Aline Viana <alinemaciel018@gmail.com>
2021-09-21 10:49:17 -0300

    tarefa 04

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,16 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+let produto1 = parseInt(prompt("Coloque os valores do primeiro produto"))
+let produto2 = parseInt(prompt("Coloque os valores do segundo produto"))
+let produto3 = parseInt(prompt("Coloque os valores do terceiro produto"))
+let produto4 = parseInt(prompt("Coloque os valores do quarto produto"))
+let produto5 = parseInt(prompt("Coloque os valores do quinto produto"))
+let produto6 = parseInt(prompt("Coloque os valores do sexto produto"))
+let produto7 = parseInt(prompt("Coloque os valores do sétimo produto"))
+let produto8 = parseInt(prompt("Coloque os valores do oitavo produto"))
+let produto9 = parseInt(prompt("Coloque os valores do nono produto"))
+let produto10 = parseInt(prompt("Coloque os valores do décimo produto"))
+let result = produto1+produto2+produto3+produto4+produto5+produto6+produto7+produto8+produto9+produto10
+alert(result)
