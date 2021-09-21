16dc48e3da6a976c5f66cae32c9e28e1a5efebbb
Aline Viana <alinemaciel018@gmail.com>
2021-09-21 14:46:44 -0300

    tarefa 05

--- exercicios-para-casa/tarefa-05/script.js
+++ exercicios-para-casa/tarefa-05/script.js
@@ -5,3 +5,9 @@
     2. Receba o valor de cada um dos itens da compra;
     3. Informe o total gasto na compra.
  */
+let item = prompt("Quantos itens você adquiriu?")
+let valorDosGastos = parseInt(prompt("Qual o valor desse produtos?"))
+
+
+
+
