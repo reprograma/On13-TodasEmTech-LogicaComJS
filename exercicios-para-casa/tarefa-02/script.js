c2e229a4c6170f340667c02a82f44e6ec42eb846
tainaconti <tainawandermurem@hotmail.com>
2021-09-22 10:40:54 -0300

    Entrega dos exercícios

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -6,3 +6,15 @@
     3. Informe ao usuário o valor da mercadoria após o desconto;
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
+
+let mercadoriaPreco = Number(prompt("Insira o preço da mercadoria"));
+let desconto = Number(prompt("Insira o percentual do desconto"));
+
+let resultado = (mercadoriaPreco * desconto)/100; 
+alert("O valor da mercadoria é:" + resultado);
+
+let quantiaEconomizada = (mercadoriaPreco - resultado);
+alert("A quantia economizada foi de: " + quantiaEconomizada);
+
+
+
