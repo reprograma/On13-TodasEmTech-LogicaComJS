5e2d695e2fa441c131f92f403e7751a19ba47e1a
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-09-22 11:31:56 -0300

    enviando tarefa 04

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -1,6 +1,20 @@
-/**
-    Tarefa 04
+   /**
+       Tarefa 04
 
-    1. Receba o preço de cada um dos 10 itens;
-    2. Mostre na tela a soma de todos os preços.
- */
+      1. Receba o preço de cada um dos 10 itens;
+      2. Mostre na tela a soma de todos os preços.
+   */
+
+   //criar variável valorTotal
+   let valorTotal = 0;
+
+   for (let i = 0; i < 10; i++) {
+      console.log("Passo " + i);
+      let valorProduto = parseFloat(prompt("Digite o valor do produto " + i));
+      console.log("Valo do produto " + valorProduto);
+      valorTotal = (valorTotal + valorProduto);
+      console.log("Valor total atualizado " + valorTotal);
+      alert(`O valor total é ${valorTotal}`);
+   }
+
+   
