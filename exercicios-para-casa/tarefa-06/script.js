e8c314177aa079affede9671b67db16e1f85fd34
AidaFig <aidafigueiredo8@gmail.com>
2021-09-24 20:10:41 -0300

    Tarefa 6 feita

--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,12 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+    let money = Number(prompt("Insira a quantia em dinheiro dada pelo clente"));
+
+    let total = Number(prompt("Insira o total da compra do cliente"));
+
+    let change = money - total;
+    console.log(change)
+
+    alert(`O valor do troco deve ser ${change}`)
