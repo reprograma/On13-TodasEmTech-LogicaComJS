953e1bb1c54a99e5b58f14582a184493e298b871
Quezia Guilhermina Guilhermina <queziagds@gmail.com>
2021-09-24 20:52:56 -0300

    Envio das das atividades 3, 4, 5 e 6

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -7,6 +7,12 @@
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
 
-    let preço = prompt("Digite o preço.");
+   /* let preço = prompt("Digite o preço.");
     console.log(preço);
+    let desconto.[]*/
     
+    let preco = prompt("Informe o valor do produto."); 
+    let desconto = prompt("Informe o valor do desconto"); 
+    let finalPreco = parseInt(preco) /100; let total = finalPreco * parseInt(desconto);
+    let novoPreco = preco - total; let aviso = alert("O valor do produto é" + novoPreco + "reais"); 
+    let economize = alert("Você economizou"  + total+ "  reais");
--- exercicios-para-casa/tarefa-03/script.js
+++ exercicios-para-casa/tarefa-03/script.js
@@ -6,3 +6,8 @@
       - caso seja, informe na tela que ela pode comprar bebidas;
       - caso contrário, informe que você não está autorizada a vender álcool para menores.
  */
+      let ano = prompt("Digite o ano do seu nascimento");
+      if(2021-ano >=18){ alert("Pode comprar bebida")
+      } else{
+        alert("Proibido vender bebida alcóolica para menores de idade")
+      }
