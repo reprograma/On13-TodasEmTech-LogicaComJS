4f9038381e31e5ca214d3f5b5dc2585c174220d0
Taina <taiaguilera@gmail.com>
2021-09-24 20:00:37 -0300

    finalizando, faltando atividades por falta de compreensão.

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -6,9 +6,20 @@
     3. Informe ao usuário o valor da mercadoria após o desconto;
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
-    let preco;
-    for (preco = 1 ; preco <= 10; preco ++){
-        console.log('preco da mercadoria' + preco);
-    }
+    let a = (prompt('Valor Melancia'));
+let b = (prompt('Valor Umbu'));
 
-    for(i = 1; i <= 10)
+let descontoMelancia = 2;
+let descontoUmbu = 3;
+let mod = a % descontoMelancia ;
+    alert(`Desconto na Melancia de ${descontoMelancia} % ${a} = ${mod}`);
+
+let mod2 = b % descontoUmbu;
+    alert(`Desconto no Umbu de ${descontoUmbu} % ${b} = ${mod2}`);
+  let div = a - descontoMelancia;
+  alert(`Você economizou a quantia de ${a} - ${descontoMelancia} = ${div}`);
+
+  let div2 = b - descontoUmbu;
+  alert(`Você economizou a quantia de ${b} - ${descontoUmbu} = ${div2}`); 
+
+   alert(`Obrigada e volte sempre!`);
--- exercicios-para-casa/tarefa-03/script.js
+++ exercicios-para-casa/tarefa-03/script.js
@@ -1,23 +1,18 @@
-
-    Tarefa 03
+/*Tarefa 03
 
     1. Solicite o ano de nascimento;
-let nome = 'Taiana';
-console.log(nome);
-
-
-    2. Verifique se a pessoa é maior de idade:
+    //2. Verifique se a pessoa é maior de idade:
       - caso seja, informe na tela que ela pode comprar bebidas;
       - caso contrário, informe que você não está autorizada a vender álcool para menores.
  */
 
-      let anoNascimento = prompt("Qual o ano de seu Nascimento?").toLowerCase();
+  let anoNascimento = prompt("Qual o ano de seu Nascimento?").toLowerCase();
 switch (numeroEscolhido) {
 
-let anoNascimento = 2005;
+  let anoNascimento = 2005;
 
 // Condição
-if (anoNascimento <= 2004) {
+  if (anoNascimento <= 2004) {
   // Ação
   alert("Lamentamos, mas devido à sua idade não podemos vender bebidas alcoólicas");
 } else if (anoNascimento >= 2005) {
@@ -26,3 +21,14 @@ if (anoNascimento <= 2004) {
 } else {
 alert("Não achei sua condição.");
 }
+
+
+  let idade = 18;
+console.log (idade);
+  let acesso = idade >= 18;
+
+  if (acesso >= 18){
+alert('Permitido consumo de bebida para maiores de 18 anos');
+} else if ( acesso< 18) {
+  alert('Não Permitido consumo de bebida para menores de 18 anos');
+  }
