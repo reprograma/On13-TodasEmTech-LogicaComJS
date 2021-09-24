f590445aa7930865ad8b747c56d69da4d21a3494
AidaFig <aidafigueiredo8@gmail.com>
2021-09-24 20:10:32 -0300

    Tarefa 5 feita

--- exercicios-para-casa/tarefa-05/script.js
+++ exercicios-para-casa/tarefa-05/script.js
@@ -5,3 +5,11 @@
     2. Receba o valor de cada um dos itens da compra;
     3. Informe o total gasto na compra.
  */
+
+    let quantity = Number(prompt("Insira a quantidade de itens da sua compra"));
+
+    let price = Number(prompt("Insira o valor dos itens"));
+
+    let product = price * quantity;
+
+    alert(product);
