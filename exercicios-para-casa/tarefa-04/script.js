fba91e04bbf394ed695426f2376752eccff39092
AidaFig <aidafigueiredo8@gmail.com>
2021-09-24 20:09:40 -0300

    Tarefa 4 feita

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,18 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+    let price1 = prompt("Insira o preço do primeiro produto");
+    let price2 = prompt("Insira o preço do primeiro produto");
+    let price3 = prompt("Insira o preço do primeiro produto");
+    let price4 = prompt("Insira o preço do primeiro produto");
+    let price5 = prompt("Insira o preço do primeiro produto");
+    let price6 = prompt("Insira o preço do primeiro produto");
+    let price7 = prompt("Insira o preço do primeiro produto");
+    let price8 = prompt("Insira o preço do primeiro produto");
+    let price9 = prompt("Insira o preço do primeiro produto");
+    let price10 = prompt("Insira o preço do primeiro produto");
+
+    let soma =
+    
+    for ()
