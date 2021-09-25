9ed1e63f7107efef3e2bc730b7fa09bc63a13898
Daniele <daniszcode>
2021-09-24 21:28:13 -0300

    Atividade finalizada

--- exercicios-para-casa/tarefa-03/script.js
+++ exercicios-para-casa/tarefa-03/script.js
@@ -6,3 +6,11 @@
       - caso seja, informe na tela que ela pode comprar bebidas;
       - caso contrário, informe que você não está autorizada a vender álcool para menores.
  */
+
+const nascimento = Number(prompt("Digite o ano de seu nascimento: "));
+const idade = 2021 - nascimento;
+if (idade >= 18) {
+  alert("Você pode comprar bebidas");
+} else {
+  alert("Você não pode comprar bebidas");
+}
--- exercicios-para-casa/tarefa-06/script.js
+++ exercicios-para-casa/tarefa-06/script.js
@@ -5,3 +5,9 @@
     2. Informe o total da conta do cliente;
     3. Exiba uma mensagem com o valor do troco.
  */
+
+const quantia = Number(prompt("Quantia em dinheiro paga: "));
+const conta = Number(prompt("Valor da conta: "));
+
+const total = quantia - conta;
+alert(`Seu troco é: ${total} R$`);
