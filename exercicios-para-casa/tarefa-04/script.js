de5d67ee1333b1196702a6eb13758988bb88b5ae
jaíne oliveira <carineaparecida54@gmail.com>
2021-09-25 09:33:14 -0300

    Exercicio da semana de js

--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,19 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+
+
+let preços = [];
+let somaPreços = 0;
+
+for (let = 1 - 0; 1++); {
+    preços.push = (Number("digite o valor")));
+};
+
+for (let item - 0; item < preços.lengt; item++); {
+    somaPreços + -preços[item];
+
+}
+
+alert("A soma total é:" + somaPreços);
