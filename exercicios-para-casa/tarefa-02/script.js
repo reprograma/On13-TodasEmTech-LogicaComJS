a59f66d2214e0ad59295149f50e27bd6b0393cb0
unknown <engsamaranascimento@gmail.com>
2021-09-24 22:57:15 -0300

    exercício de javascript

--- exercicios-para-casa/tarefa-02/script.js
+++ exercicios-para-casa/tarefa-02/script.js
@@ -6,3 +6,5 @@
     3. Informe ao usuário o valor da mercadoria após o desconto;
     4. Informe ao usuário qual quantia foi economizada por conta do desconto.
  */
+
+   
--- exercicios-para-casa/tarefa-04/script.js
+++ exercicios-para-casa/tarefa-04/script.js
@@ -4,3 +4,5 @@
     1. Receba o preço de cada um dos 10 itens;
     2. Mostre na tela a soma de todos os preços.
  */
+
+    
